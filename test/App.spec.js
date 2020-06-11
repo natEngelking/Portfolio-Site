379bbe864eb79fa1bb8fbdab63fd589fdd0c4bc9
natEngelking <natalie.mixdorf@gmail.com>
2020-06-11 17:23:41 -0600

    got jest installed

---
+++ test/App.spec.js
@@ -0,0 +1,19 @@
+//import React from 'react';
+//import Enzyme, { shallow } from 'enzyme'; 
+//import Adapter from 'enzyme-adapter-react-16';
+//import App from '../app.jsx';
+
+//enzyme configuration
+// Enzyme.configure({ adapter: new Adapter() });
+
+
+
+//test
+
+// describe('App', () => {
+//   it('should show text', () => {
+//     const wrapper = shallow(<App />);
+//     const text = wrapper.find('div');
+//     expect(text.text()).toBe('Creation Station');
+//   })
+// })
